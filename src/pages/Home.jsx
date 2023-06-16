45e4d54dcc7a41e905e5fe7bdea45cd85588f459
dinbusan <deann.videtto@gmail.com>
2023-06-16 13:47:20 +0200

    added a loading screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,37 +4,51 @@ import Table from "../Table";
 function MyComponent() {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
-
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(`https://swapi.dev/api/people/?page=${page}`);
-      const jsonData = await response.json();
-      setData(jsonData.results);
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `https://swapi.dev/api/people/?page=${page}`
+        );
+        const jsonData = await response.json();
+        setData(jsonData.results);
+      } catch (error) {
+        console.error("There was an error:", error);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchData();
   }, [page]);
 
-      const handleNext = () => {
-        setPage(page + 1);
-      };
+  const handleNext = () => {
+    setPage(page + 1);
+  };
 
-      const handlePrev = () => {
-        setPage(page - 1);
-      };
-
+  const handlePrev = () => {
+    setPage(page - 1);
+  };
 
   return (
     <div className="App">
       <h1>Star Wars Characters</h1>
-      <Table data={data} />
-      <button onClick={handlePrev} disabled={page === 1}>
-        Prev Page
-      </button>
-      <button onClick={handleNext} disabled={page === 9}>
-        Next Page
-      </button>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <Table data={data} />
+          <button onClick={handlePrev} disabled={page === 1}>
+            Prev Page
+          </button>
+          <button onClick={handleNext} disabled={page === 9}>
+            Next Page
+          </button>
+        </>
+      )}
     </div>
   );
 }
