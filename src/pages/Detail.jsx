822a50851f8dceee423c4188c4c3b257d20999d4
dinbusan <deann.videtto@gmail.com>
2023-06-18 13:04:22 +0200

    added a go home button on the detail page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const Details = ({ data }) => {
   const { id } = useParams();
@@ -13,15 +13,20 @@ const Details = ({ data }) => {
 
   return (
     <div>
-      <h2>Character Details</h2>
-      <p>Name: {character.name}</p>
-      <p>Birth Year: {character.birth_year}</p>
-      <p>Species: {character.species}</p>
-      <p>Height: {character.height}</p>
-      <p>Mass: {character.Mass}</p>
-      <p>Skin Color: {character.skin_color}</p>
-      <p>Films: {character.films.join(", ")}</p>
-      <p>Homeworld: {character.homeworld}</p>
+      <div>
+        <h2>Character Details</h2>
+        <p>Name: {character.name}</p>
+        <p>Birth Year: {character.birth_year}</p>
+        <p>Species: {character.species}</p>
+        <p>Height: {character.height}</p>
+        <p>Mass: {character.Mass}</p>
+        <p>Skin Color: {character.skin_color}</p>
+        <p>Films: {character.films.join(", ")}</p>
+        <p>Homeworld: {character.homeworld}</p>
+      </div>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
     </div>
   );
 };
