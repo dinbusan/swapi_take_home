f195f709da127f19fc1b38a003db05fda74f0c18
dinbusan <deann.videtto@gmail.com>
2023-06-18 19:26:26 +0200

    moved browswer router to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import Body from "./pages/Body";
 
 const App = () => {
   return (
-    <Router>
+   
       <div>
         <h1>Star Wars Characters</h1>
         <Body />
       </div>
-    </Router>
+  
   );
 };
 
